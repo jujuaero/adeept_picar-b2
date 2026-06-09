947d3f32d021e726125e8adb7264190aa44c56cf
Max <max.dambron@efrei.net>
2026-06-09 14:26:11 +0200

    tache5

---
+++ robot_team_xy/05_ultrasond.py
@@ -0,0 +1,16 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+Tr = 23
+Ec = 24
+sensor = DistanceSensor(echo=Ec, trigger=Tr,max_distance=2) 
+
+# Get the distance of ultrasonic detection.
+def checkdist():
+    return (sensor.distance) *1000 # Unit: mm
+
+if __name__ == "__main__":
+    while True:
+        distance = checkdist() 
+        print("%.2f mm" %distance)
+        sleep(0.05)
