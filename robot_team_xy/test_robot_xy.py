b72cc0707f7111bd9df6f121496ccfcde7556bfb
jules <92889571+jujuaero@users.noreply.github.com>
2026-06-09 10:12:51 +0200

    Add files via upload

---
+++ robot_team_xy/test_robot_xy.py
@@ -0,0 +1,69 @@
+import RPi.GPIO as GPIO
+import time
+
+def switchSetup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    # 3 LED BASIQUE
+    GPIO.setup(9, GPIO.OUT)
+    GPIO.setup(25, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    # 6 LED RGB
+    # Gauche
+    GPIO.setup(0, GPIO.OUT)
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+    # Droite
+    GPIO.setup(1, GPIO.OUT)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(6, GPIO.OUT)
+
+
+LEDS = {
+    1: (9, False),
+    2: (25, False),
+    3: (11, False),
+    4: (13, True),
+    5: (19, True),
+    6: (0, True),
+    7: (1, True),
+    8: (5, True),
+    9: (6, True)
+}
+
+
+def light_on(LED):
+    if LED[1]:
+        GPIO.output(LED[0], GPIO.LOW)
+    else:
+        GPIO.output(LED[0], GPIO.HIGH)
+
+
+def light_off(LED):
+    if LED[1]:
+        GPIO.output(LED[0], GPIO.HIGH)
+    else:
+        GPIO.output(LED[0], GPIO.LOW)
+
+
+def set_all_switch_off():
+    for i in LEDS:
+        light_off(LEDS[i])
+
+
+if __name__ == "__main__":
+    cmd = 0
+    switchSetup()
+    set_all_switch_off()
+    while cmd >= 0:
+        cmd = int(input("Choose between 11 and 29 or -1 to quit: "))
+        if 11 <= cmd <= 19:
+			      light_on(LEDS[cmd - 10])
+        if 21 <= cmd <= 29:
+            light_off(LEDS[cmd - 20])
+        if cmd == 20:
+            set_all_switch_off()
+        
+			
+
+			
